5c39211e50510a3b8209bb7ffdf1d68a4f20d695
tanishk26 <tanishkthilakan01@gmail.com>
2019-09-30 22:26:40 +0530

    TEST FILE

--- sketch_sep23b/sketch_sep23b.ino
+++ sketch_sep23b/sketch_sep23b.ino
@@ -1,14 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(9,OUTPUT);
+  pinMode(13,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(9,LOW);
+digitalWrite(13,LOW);
 delay(1500);
-digitalWrite(9,HIGH);
+digitalWrite(13,HIGH);
 delay(1000);
-digitalWrite(9,LOW);
+digitalWrite(13,LOW);
 }
